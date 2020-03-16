1605db6cee6f22e20ff8b897b6b7933d9178ac41
kongfan <54395991+yunjuan-yunshu@users.noreply.github.com>
2020-03-16 16:50:33 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -73,7 +73,7 @@ function loadWidget(config) {
 			Live2D.captureFrame = true;
 		});
 		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/stevenjoezhang/live2d-widget");
+			open("https://yunjuan-yunshu.github.io/");
 		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
